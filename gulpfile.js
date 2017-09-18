527bc8ca9479067ded411ca71d34b610fa1398d9
Karine91 <kitana.91@mail.ru>
2017-09-18 19:54:06 +0300

    about part2

--- gulpfile.js
+++ gulpfile.js
@@ -65,8 +65,8 @@ function styles(){
             errorHandler: function(error){console.log(error); this.end();}
          }))
         .pipe(sourcemaps.init())
-        .pipe( postcss([ require('precss'), require('autoprefixer')]) )
         .pipe(sass({outputStyle: 'compressed'}))
+        .pipe( postcss([ require('precss'), require('autoprefixer')]) )
         .pipe(sourcemaps.write())
         .pipe(rename({suffix: '.min'}))
         .pipe(gulp.dest(paths.styles.dest))
