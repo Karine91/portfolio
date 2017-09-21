f7a605ad625a5d172728cc4c18ec305a5beedb2d
Karine91 <kitana.91@mail.ru>
2017-09-21 17:41:32 +0300

    add menu show

--- src/scripts/common/menu.js
+++ src/scripts/common/menu.js
@@ -1,9 +1,22 @@
 let trigger = $('#menu-button');
-
+let wrapper = $('.page-menu-wrapper');
 var initMenu = () => {
     if(trigger){
         trigger.on('click', function(){
-            $(this).find('.hamburger-menu-icon').toggleClass('open');
+            var that = $(this);
+            var menu = that.find('.hamburger-menu-icon'); 
+            if (menu.hasClass('open')){
+                menu.removeClass('open');
+                wrapper.hide();
+            }else{
+                menu.addClass('open');
+                wrapper.show();
+                wrapper.on('mousewheel', function(e){
+                    e.preventDefault();
+                });              
+            }
+           
+            
         });
     }  
 };
