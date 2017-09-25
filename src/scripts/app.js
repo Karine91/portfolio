8394aaad4b57539fb61e973698ca10f579a23c18
Karine91 <kitana.91@mail.ru>
2017-09-26 01:22:01 +0300

    blur is working

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -3,9 +3,11 @@ const slider = require('./common/slider');
 const authFlip = require('./common/authorization');
 const gmap = require('./common/gmaps.js');
 const menu = require('./common/menu.js');
+const blur = require('./common/blur.js');
 $(document).ready(function(){
     gmap();
     slider();
     authFlip();
+    blur();
     menu();
 });
---
+++ src/scripts/common/blur.js
@@ -0,0 +1,42 @@
+var blur =(function(){
+    var wrapper = document.querySelector('.contacts-form'),
+        form = document.querySelector('.blur');
+        return {
+           set: function(){
+               var  windowWidth = document.querySelector('.section-content_testimonials').offsetWidth,
+                    windowHeight = document.querySelector('.section-content_testimonials').offsetHeight,
+                    posLeft = -wrapper.offsetLeft,
+                    posTop =  -wrapper.offsetTop,
+                    imgWidth = 2000,
+                    imgHeight = 1358;
+                var imageRatio =imgWidth/imgHeight;
+                var coverRatio = windowWidth/windowHeight;
+                var coverWidth, coverHeight, scale;
+                    if (imageRatio >= coverRatio) {
+                        coverHeight = windowHeight;
+                        scale = (coverHeight / imgHeight);
+                        coverWidth =imgWidth * scale;
+                    } else {
+                        coverWidth = windowWidth;
+                        scale = (coverWidth / imgWidth);
+                        coverHeight = imgHeight * scale;
+                    }
+          
+                        var blurCss = form.style;
+               blurCss.backgroundSize = coverWidth + 'px' + " " + coverHeight + 'px';
+               blurCss.backgroundPosition = posLeft + 'px' + " " + posTop + 'px';
+            }, 
+        };
+
+}());
+
+function init(){
+    blur.set();
+    window.onresize = function(){
+        blur.set();
+    };
+}
+
+
+
+module.exports = init;
