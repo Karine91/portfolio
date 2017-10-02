ff8f8b222b2da842f764a21880bd64eff9be3d81
Karine91 <kitana.91@mail.ru>
2017-10-03 02:35:50 +0300

    scroll v2

--- src/scripts/common/sidebarScroll.js
+++ src/scripts/common/sidebarScroll.js
@@ -2,88 +2,25 @@ const sidebar = document.querySelector('.sidebar');
 const blog = document.querySelector('.blog');
 const links = sidebar.querySelectorAll('.post-list__link ');
 
-//main animate function
-function animate(options) {
-    var start = performance.now();
-    requestAnimationFrame(function animate(time) {
-        var timeFraction = (time - start) / options.duration;
-        if (timeFraction > 1) timeFraction = 1;
-        var progress = options.timing(timeFraction);
-        options.draw(progress);
-        if (timeFraction < 1) {
-            requestAnimationFrame(animate);
-        }
-    });
-}
-//Timing functions
-function linear(timeFraction) {
-    return timeFraction;
-}
-function easeInQuad(timeFraction) {
-    return Math.pow(timeFraction, 2);
-}
-function easeInOct(timeFraction) {
-    return Math.pow(timeFraction, 4);
-}
-function easeInCirc(timeFraction) {
-    return 1 - Math.sin(Math.acos(timeFraction));
-}
-function easeInBack(timeFraction) {
-    var x = 1.5;
-    return Math.pow(timeFraction, 2) * ((x + 1) * timeFraction - x);
-}
-function easeInBounce(timeFraction) {
-    for (var a = 0, b = 1, result; ; a += b, b /= 2) {
-        if (timeFraction >= (7 - 4 * a) / 11) {
-            return -Math.pow((11 - 6 * a - 11 * timeFraction) / 4, 2) + Math.pow(b, 2);
-        }
-    }
-}
-function easeInElastic(timeFraction) {
-    var x = 1.5;
-    return Math.pow(2, 10 * (timeFraction - 1)) * Math.cos(20 * Math.PI * x / 3 * timeFraction);
-}
-//Reverse functions
-function makeEaseOut(timing) {
-    return function(timeFraction) {
-        return 1 - timing(1 - timeFraction);
-    };
-}
-var easeOutQuad = makeEaseOut(easeInQuad);
-var easeOutOct = makeEaseOut(easeInOct);
-var easeOutCirc = makeEaseOut(easeInCirc);
-var easeOutBack = makeEaseOut(easeInBack);
-var easeOutBounce = makeEaseOut(easeInBounce);
-var easeOutElastic = makeEaseOut(easeInElastic);
-//In-Out functions
-function makeEaseInOut(timing) {
-    return function(timeFraction) {
-        if (timeFraction < .5)
-            return timing(2 * timeFraction) / 2;
-        else
-            return (2 - timing(2 * (1 - timeFraction))) / 2;
-    };
-}
-var easeInOutQuad = makeEaseInOut(easeInQuad);
-var easeInOutOct = makeEaseInOut(easeInOct);
-var easeInOutCirc = makeEaseInOut(easeInCirc);
-var easeInOutBack = makeEaseInOut(easeInBack);
-var easeInOutBounce = makeEaseInOut(easeInBounce);
-var easeInOutElastic = makeEaseInOut(easeInElastic);
-
 function spySidebarLinks(winScroll){
+    var V = 0.65;
     for(var i=0; i<links.length; i++){
         links[i].addEventListener('click', function(event){
             var id = this.getAttribute('href');
-            var y = document.querySelector(id).offsetTop;
+            var y = document.querySelector(id).getBoundingClientRect().top;
             var delta = y - winScroll;
-            animate({
-                duration: 1000,
-                timing: easeOutOct,
-                draw: function(progress){
-                    document.body.scrollTop = winScroll + delta*progress;
-                },
-            });
+            var start = null;
+            //document.body.scrollTop = winScroll + delta;
+            requestAnimationFrame(step);
+            function step(time) {
+              if (start === null) start = time;
+              var progress = time - start,
+                  r = (y < 0 ? Math.max(winScroll - progress/V, winScroll + y) : Math.min(winScroll + progress/V, winScroll + y));
+              window.scrollTo(0,r);
+              if (r != winScroll + y) {
+                requestAnimationFrame(step);
+              }
+            } 
             
             event.preventDefault();
         });
